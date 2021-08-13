47aec90aca959ffd84836d7f2aa7379041ff2a43
igoragatti <igoragatti@2824597.no-reply.drupal.org>
2021-08-13 11:42:15 -0300

    Add form event table to UI and delete funcionality

--- forms.php
+++ forms.php
@@ -14,83 +14,39 @@ $PAGE->set_heading('Mautic Administration');
 $form_createsettings = new settings_form();
 $datalib = new \local_mautic\lib\datalib();
 
+$renderer = $PAGE->get_renderer('local_mautic');
+
+$action = optional_param('action', '', PARAM_ALPHAEXT);
+$formeventid = optional_param('id', '', PARAM_RAW);
+
+if ($formeventid) {
+    $formevent = $datalib->getformevent($formeventid);
+}
+
 if ($data = $form_createsettings->get_data()) {
     try {
-        //Create
-        $datalib->create_eventlink($data);
+        $datalib->createformevent($data);
         redirect($PAGE->url, get_string('changessaved'), null, \core\output\notification::NOTIFY_SUCCESS);
     } catch (Exception $e) {
         redirect($PAGE->url, $e->getMessage(), null, \core\output\notification::NOTIFY_ERROR);
     }
+} else if ($action == 'delete') {
+    if (!optional_param('confirm', false, PARAM_BOOL)) {
+        $continueparams = ['action' => 'delete', 'id' => $formeventid, 'sesskey' => sesskey(), 'confirm' => true];
+        $continueurl = new moodle_url('/local/mautic/forms.php', $continueparams);
+        $cancelurl = new moodle_url('/local/mautic/forms.php');
+        echo $OUTPUT->header();
+        echo $OUTPUT->confirm(get_string('deleteformevent', 'local_mautic'), $continueurl, $cancelurl);
+        echo $OUTPUT->footer();
+    } else {
+        require_sesskey();
+        $datalib->deleteformevent($formeventid);
+        redirect($PAGE->url, 'Form Event deleted', null, \core\output\notification::NOTIFY_SUCCESS);
+    }
 } else {
     echo $OUTPUT->header();
     echo $OUTPUT->heading('Settings');
+    echo $renderer->show_events();
     $form_createsettings->display();
     echo $OUTPUT->footer();
 }
-
-
-
-//$renderer = $PAGE->get_renderer('local_mautic');
-
-//echo $OUTPUT->header();
-//echo $OUTPUT->heading('Create Event Integrations');
-//echo $renderer->show_events();
-//$params = ['action' => 'create'];
-//$addurl = new moodle_url("$CFG->wwwroot/local/mautic/forms.php", $params);
-//echo $renderer->single_button($addurl, get_string('createnewcoupon', 'enrol_coupon'));
-//echo $OUTPUT->footer();
-
-//$datalib = new \enrol_coupon\data\datalib();
-
-//if ($couponid) {
-//  $coupon = $datalib->read_coupon_value(array('id' => $couponid));
-//  $coursesids = $datalib->get_courses_ids($coupon->id);
-//  $courseslist = array();
-//  foreach ($coursesids as $obj) {
-//    array_push($courseslist, $obj->courseid);
-//  }
-
-//  $form_createcoupon = new coupon_form($coupon, $courseslist);
-//  if (!$coupon) {
-//    print_error('invaliddata');
-//  }
-//}
-
-//if ($action == 'create') {
-//  echo $OUTPUT->header();
-//  echo $OUTPUT->heading('Create Coupons');
-//  $form_createcoupon->display();
-//  echo $OUTPUT->footer();
-//} else if ($action == 'delete') {
-//  if (!optional_param('confirm', false, PARAM_BOOL)) {
-//    $continueparams = ['action' => 'delete', 'id' => $couponid, 'sesskey' => sesskey(), 'confirm' => true];
-//    $continueurl = new moodle_url('/enrol/coupon/coupons.php', $continueparams);
-//    $cancelurl = new moodle_url('/enrol/coupon/coupons.php');
-//    echo $OUTPUT->header();
-//    echo $OUTPUT->confirm(get_string('deletecoupon', 'enrol_coupon', s($coupon->code)), $continueurl, $cancelurl);
-//    echo $OUTPUT->footer();
-//  } else {
-//    require_sesskey();
-//    $datalib->delete_coupon($couponid);
-//    redirect($PAGE->url, 'Coupon deleted', null, \core\output\notification::NOTIFY_SUCCESS);
-//  }
-//} else {
-//  if ($data = $form_createcoupon->get_data()) {
-//    try {
-//      //Create
-//      $datalib->create_coupon($data);
-//      redirect($PAGE->url, get_string('changessaved'), null, \core\output\notification::NOTIFY_SUCCESS);
-//    } catch (Exception $e) {
-//      redirect($PAGE->url, $e->getMessage(), null, \core\output\notification::NOTIFY_ERROR);
-//    }
-//  } else {
-//    echo $OUTPUT->header();
-//    echo $OUTPUT->heading('Create Coupons');
-//    echo $renderer->show_coupons();
-//    $params = ['action' => 'create'];
-//    $addurl = new moodle_url("$CFG->wwwroot/enrol/coupon/coupons.php", $params);
-//    echo $renderer->single_button($addurl, get_string('createnewcoupon', 'enrol_coupon'));
-//    echo $OUTPUT->footer();
-//  }
-//}
